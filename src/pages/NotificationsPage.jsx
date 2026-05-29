84b0f0d1f689e511994d13a25e2481c297bb6335
Freia123 <freia@mandrupkrog.dk>
2026-05-29 11:22:26 +0200

    header sat ind i notifikations side

--- src/pages/NotificationsPage.jsx
+++ src/pages/NotificationsPage.jsx
@@ -1,8 +1,10 @@
+import Header from "../components/Header";
 import ErrorMessage from "../components/Errormessage";
 
 export default function NotificationsPage() {
   return (
     <main>
+      <Header title="Notifikationer" />
       <ErrorMessage />
     </main>
   );
