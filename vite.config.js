c761547f87b86f472c2f3c03b98eb9aaea744321
eaa25cmc <eaa25cmc@students.eaaa.dk>
2026-06-02 12:03:10 +0200

    filer lavet og filer ændret

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import pkg from "./package.json";
 
-export default defineConfig({
-  plugins: [react()],
-  base: "/webapp-eksamensprojekt/",
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    base: command === "serve" ? "/" : pkg.base,
+  };
 });
